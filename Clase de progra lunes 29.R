c1f855f87a0516890865d2bf8704280f4620605c
Agustin Diaz <agus_gok@hotmail.com>
2016-08-31 09:43:30 -0500

    Se agregó una primer base de datos
    Se descargo de Yahho finance la base de datos de precios de S&P y se
    utilizó para ejemplificar el uso de red.table

--- Clase de progra lunes 29.R
+++ Clase de progra lunes 29.R
@@ -28,3 +28,64 @@ m1
 m2 <- cbind(m,y)
 m2
 
+#Factores 
+x <- factor(c("si","no","si","si","no","si","no"))
+x
+unclass(x)
+x <- factor(c("si","no","si","si","no","si","no")) levels= c("si","no")
+x <- factor(c("azul","azul","rojo","azul","amarillo","verde", "azul"))
+x
+table(x)
+
+
+#Valores Faltantes
+x <-c(1,2,NA,10,3)
+is.na(x)
+is.nan(x)
+
+x <-c(1,2,NaN,10,3)
+is.na(x)
+is.nan(x)
+
+#Data frames
+x <- data.frame(Erick=1:4, Lori= c(T,T,F,F))
+row.names(x) <- c("Primero","segundo","Tercero","Cuarto")
+x
+nrow(x)
+ncol(x)
+attributes(x)
+names(x) <- c("Yarely", "Karen")
+x
+
+#Los nombres no son exclusivos de los datas frames
+x <- 1:3
+names(x) #NULL
+names(x) <- c("Hugo", "Paco", "Luis")
+x
+
+x <- list(a=1:10, b=100:91,c=51:60)
+x
+names(x) <- c("Seq1", "Seq2", "Seq3")
+x
+
+
+m<- matrix(1:4, 2,2)
+m
+attributes(m)
+dimnames(m) <- list(c("fil1","fil2"),c("col1","col2"))
+m
+
+z <- matrix(NA,6,6)
+z
+attributes(z)
+dimnames(z) <- list(c("1","2","3","4","5","6"),c("A","B","C","D","E","F"))
+z
+
+getwd()
+setwd("~/GitHub/Pogramacion_Actuarial_III_OT16")
+data <- read.csv("Datos_S&P.csv")
+data <-read.table("Datos_S&P.csv",T,",")
+data
+
+
+
